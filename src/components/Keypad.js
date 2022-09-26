e8155779a0b6f7716fc56e5598dc1a124d90ac87
Wilson-Ochieng <wilsonochieng718@gmail.com>
2022-09-26 08:42:19 +0300

    Assignment Completed

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,19 @@
-// Code Keypad Component Here
 
+// Code EyesOnMe Component Here
 function Keypad (){
+    const OnchangeHandler = (event) =>{
+        console.log('Entering password...')
+    }
     return (
-        <div></div>
+        <input 
+            type="password" 
+            name="password" 
+            id="password"
+            onChange={OnchangeHandler}
+         />
     )
 }
 
 export default Keypad;
+
+
